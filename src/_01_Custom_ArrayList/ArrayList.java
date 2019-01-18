a4af6f57e4ad75c004e5a0595b9036d713689bbc
GeneralOy <league@WorthyMini-5.attlocal.net>
2019-01-17 16:58:53 -0800

    wow - work on the first thing, custom arraylist

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -2,34 +2,55 @@ package _01_Custom_ArrayList;
 
 @SuppressWarnings("unchecked")
 
-public class ArrayList <T>{
-	
+public class ArrayList<T> {
+	T[] List;
+	int r = 0;
 	public ArrayList() {
+		
 	}
-	
+
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		return List[loc];
 	}
-	
+
 	public void add(T val) {
-		
+			List[List.length] = val;
+			//if(r != List.length) {
+			//	r = List.length;
+			//}
+
 	}
-	
+
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		if (List.length >= 2) {
+			for (int i = List.length; i >= loc; i--) {
+				List[i + 1] = List[i];
+				if (i == loc) {
+					List[i] = val;
+				}
+			}
+		}
 	}
-	
+
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		List[loc] = val;
 	}
-	
+//*********
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+		for (int i = loc; i <= List.length; i++) {
+			List[i + 1] = List[i];
+			if (i == loc) {
+				List[i] = val;
+			}
+		}
 	}
-	
+//**********
 	public boolean contains(T val) {
-		
+
 		return false;
+	}
+
+	public int size() {
+		return List.length;
 	}
 }
