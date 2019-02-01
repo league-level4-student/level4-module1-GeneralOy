830769ec4abaefb982e24c84235b24cf05fd742a
GeneralOy <league@WorthyMini-5.attlocal.net>
2019-01-31 16:59:32 -0800

    asdf

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -10,6 +10,29 @@ public class CustomButtonOptionPanes {
 				options, 0);
 
 		String choice = options[input];
+		switch(choice) {
+		case "Sunday":
+			System.out.println("Sundays aren't good D:");
+			break;
+		case "Monday":
+			System.out.println("Mondays are terrible ):<");
+			break;
+		case "Tuesday":
+			System.out.println("Tuesdays suck ):");
+			break;
+		case "Wednesday":
+			System.out.println("Wednesdays are meh |:");
+			break;
+		case "Thursday":
+			System.out.println("Thursdays are okay :|");
+			break;
+		case "Friday":
+			System.out.println("Fridays are great :)");
+			break;
+		case "Saturday":
+			System.out.println("Saturdays are amazing :D");
+			break;
+		}
 		
 		//use a switch statement to do something cool for each option
 		
